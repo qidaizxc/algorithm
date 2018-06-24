4e608ce6df174f8286cb091c67fad3fa163c9549
qidaizxc <575650532@qq.com>
2018-06-24 22:58:53 +0800

    first commit

---
+++ src/main/java/com/wutong/Bascis.java
@@ -0,0 +1,12 @@
+package com.wutong;
+
+public class Bascis {
+  public static int gcd(int p, int q){
+    if(q == 0) return  p;
+    int r = p % q;
+    return  gcd(q, r);
+  }
+  public static void main(String[] args) {
+    System.out.println(gcd(13, 7));
+  }
+}
